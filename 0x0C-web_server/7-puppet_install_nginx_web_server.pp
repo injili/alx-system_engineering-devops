5a24c37525934a9fcefc5e362855c98342f625b7
injili <nyarekigospel@gmail.com>
2023-08-31 05:44:03 -0700

    kankuro and I, same clan

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,21 +7,14 @@ package { 'nginx':
 
 # Configure index.html
 file { '/var/www/html/index.html':
-	ensure => file,
+	ensure  => file,
 	content => 'Hello World!',
-	require => Package['nginx'],
-}
-
-# Configure custom_404
-file { '/var/www/html/custom_404.html':
-	ensure => file,
-	content => "Ceci n'est pas une page\n",
 	require => Package['nginx'],
 }
 
 # Configure redirect_me
 file { '/etc/nginx/sites-available/default':
-	ensure => file,
+	ensure  => file,
 	content => "
 server {
 	listen 80;
@@ -29,31 +22,24 @@ server {
 
 	server_name _;
 
-	root /var/www/html;
-	index index.html index.htm;
+	location / {
+		root /var/www/html;
+		index index.html index.htm;
+	}
 
 	location /redirect_me/ {
 		return 301 https://github/injili;
 	}
 
-	error_page 404 /custom_404.html;
-	location = /custom_404.html {
-		internal;
-		return 404 \"Ceci n'est pas une page\\n";
-	}
-
-	location / {
-		try_files $$uri $$uri/ =404;
-	}
 }
 ",
 	require => Package['nginx'],
-	notify => Service['nginx'],
+	notify  => Service['nginx'],
 }
 
 # Restart Nginx
 service { 'nginx':
-	ensure => running,
-	enable => true,
+	ensure  => running,
+	enable  => true,
 	require => Package['nginx'],
 }
