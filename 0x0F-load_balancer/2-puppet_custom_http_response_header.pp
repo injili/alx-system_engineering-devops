58d9e53b45511dbcfcc60a2b2e5fd90ada09cb51
injili <nyarekigospel@gmail.com>
2023-09-05 12:43:35 -0700

    use puppet to configure nginx response header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,23 @@
+# Puppet manifext cofiguration
+
+# Install updates and nginx
+exec { 'update':
+	command => '/usr/bin/apt-get update',
+}
+
+# install package
+package { 'nginx':
+	ensure => installed,
+}
+
+# replace file lin
+file _line { '/etc/nginx/nginx.conf':
+	path => '/etc/nginx/nginx.conf',
+	line => "http {\n\tadd_header X-Served-By \"${hostname}\";",
+	match => 'http {',
+}
+
+# restart nginx
+exec { 'start':
+	command => 'usr/sbin/service nginx start',
+}
