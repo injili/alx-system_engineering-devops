34c5ae3a6f760c5ef0843b35b7aabe67c4295a01
injili <nyarekigospel@gmail.com>
2023-10-11 03:23:32 -0700

    number of subscribers for a given subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     base_url = 'https://www.reddit.com/r/'
     url = '{}{}/about.json'.format(base_url, subreddit)
     headers = {
-            'UserAgent':
+            'User-Agent':
             'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
             Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
             }
