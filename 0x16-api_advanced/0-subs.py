25256c76b713aba2800c5089651602c2acf737af
injili <nyarekigospel@gmail.com>
2023-10-11 03:04:07 -0700

    how many subs?

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A function that querries the Reddit API and returns
+the number of subscribers for a given subreddit
+"""
+from fake_useragent import UserAgent
+import request
+
+def number_of_subscribers(subreddit):
+    """
+    If an invalid subreddit is givven the function should return 0
+    """
+    base_url = 'https://www.reddit.com/r/'
+    url = '{}{}/about.json'.format(base_url, subreddit)
+    header = {
+            'UserAgent':
+            'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+            Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
+            }
+
+    result = request.get{
+            url,
+            headers=headers
+            allow_redirect=False
+            }
+
+    if results.status_code == 200:
+        return result.json()['data']['subscribers']
+    return 0
