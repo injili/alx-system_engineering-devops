76e637f28f552374e591c89437bd29736b88b085
injili <nyarekigospel@gmail.com>
2023-11-17 03:01:32 -0800

    get the top ten hottest posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+The function top_ten prints the titles
+of the top 10 hot posts for a given subreddit
+"""
+
+
+def top_ten(subreddit):
+    """
+    Args:
+        subreddit: the subreddit to fetch the posts from
+    """
+
+    import requests
+
+    r = requests.get('https://www.reddit.com/r/{}/hot.json?limit=10'
+                     .format(subreddit),
+                     headers={'User-Agent': 'My-User-Agent'},
+                     allow_redirects=False)
+
+    if r.status_code == 200:
+        [print(child.get('data').get('title'))
+         for child in r.json().get('data').get('children')]
