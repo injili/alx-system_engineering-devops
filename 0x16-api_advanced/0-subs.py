db59a1bada7ac55f86d43a1540e379f992cd5da7
injili <nyarekigospel@gmail.com>
2023-11-17 02:24:48 -0800

    get the number of subscribers to a subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,3 +12,12 @@ def number_of_subscribers(subreddit):
     Returns the number of subscribers
     """
     import requests
+
+    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+                   headers={'User-Agent': 'My-User-Agent'},
+                   allow_redirects=False)
+
+    if r.status_code == 200:
+        return r.json().get('data').get('subscribers')
+
+    return 0
